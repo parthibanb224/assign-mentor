f523df209dede615fbc92a0ddd4f1b45d00c21ec
parthibanb224 <parthibanb224@gmail.com>
2023-07-29 09:16:35 +0530

    done

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const APP_SERVER = express();
 
+APP_SERVER.get('/', (req,res,next) => {
+    res.send("<h1>Assign-Mentor</h1>");
+});
 APP_SERVER.use('/mentors',require('./controllers/mentors.controller'));
 APP_SERVER.use('/students',require('./controllers/students.controller'));
 
